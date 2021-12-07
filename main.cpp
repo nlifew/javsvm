51dd090a2af654b85dd792ced2d9f8f8bb6ceebc
nlifew <nlifew.ss@gmail.com>
2021-12-07 18:45:08 +0800

    dev:【engine】提交测试用例

--- main.cpp
+++ main.cpp
@@ -16,17 +16,17 @@
 #include <thread>
 #include <queue>
 
-using namespace javsvm;
 
-
-int main(int argc, const char *argv[]) {
+int main() {
     chdir("..");
-    putenv(strdup("CLASSPATH=test"));
+    putenv(strdup("CLASSPATH=jdk/java"));
 
     printf("start\n");
-    jvm &vm = jvm::get();
-    vm.attach();
+//    jvm &vm = jvm::get();
+//    vm.attach();
 
+
+#if 0
     dll_loader loader;
     void *dll = loader.load_library("home/whoami/../../../../../../../Users/./././edz/desktop//////main.dylib");
     auto add = loader.find_symbol<int(*)(int, int)>("add", dll);
@@ -34,37 +34,6 @@ int main(int argc, const char *argv[]) {
     if (add) {
         printf("call add() %d\n", add(2, 3));
     }
-
-    if (true) {
-        return 0;
-    }
-
-    jclass *Main = vm.bootstrap_loader.load_class("Main");
-    jmethod *msort = Main->get_static_method("msort", "([III[I)V");
-    jmethod *qsort = Main->get_static_method("qsort", "([III)V");
-
-    jref array = vm.array.new_int_array(10);
-    int array_int[] = { 5, 8, 4, 0, 1, 3, 6, 9, 2, 7 };
-    vm.array.set_array_region(array, 0, 10, array_int);
-
-    slot_t args[4];
-    memset(args, 0, sizeof(args));
-
-    *(jref*) (args + 0) = array;
-    *(jint*) (args + 1) = 0;
-    *(jint*) (args + 2) = 9;
-    *(jref*) (args + 3) = nullptr;
-
-    jargs args_w(args);
-    run_java(qsort, nullptr, args_w);
-
-    memset(array_int, 0, sizeof(int) * 10);
-    vm.array.get_array_region(array, 0, 10, array_int);
-
-
-    for (int i : array_int) {
-        LOGI("%d\n", i);
-    }
-
+#endif
     return 0;
 }
---
+++ test/engine/engine.cpp
@@ -0,0 +1,51 @@
+//
+// Created by edz on 2021/12/7.
+//
+#include "vm/jvm.h"
+#include "object/jclass.h"
+#include "engine/engine.h"
+#include "utils/log.h"
+
+#include <unistd.h>
+
+using namespace javsvm;
+
+int main(int argc, const char *argv[])
+{
+    putenv(strdup("CLASSPATH=../../../jdk"));
+
+    LOGI("main: start\n");
+
+    jvm &vm = jvm::get();
+    vm.attach();
+
+    jclass *Main = vm.bootstrap_loader.load_class("Main");
+    assert(Main != nullptr);
+
+//    jmethod *msort = Main->get_static_method("msort", "([III[I)V");
+    jmethod *qsort = Main->get_static_method("qsort", "([III)V");
+    assert(qsort != nullptr);
+
+    jref array = vm.array.new_int_array(10);
+    int array_int[] = { 5, 8, 4, 0, 1, 3, 6, 9, 2, 7 };
+    vm.array.set_array_region(array, 0, 10, array_int);
+
+    slot_t args[4];
+    memset(args, 0, sizeof(args));
+
+    *(jref*) (args + 0) = array;
+    *(jint*) (args + 1) = 0;
+    *(jint*) (args + 2) = 9;
+    *(jref*) (args + 3) = nullptr;
+
+    jargs args_w(args);
+    run_java(qsort, nullptr, args_w);
+
+    memset(array_int, 0, sizeof(int) * 10);
+    vm.array.get_array_region(array, 0, 10, array_int);
+
+
+    for (int i : array_int) {
+        LOGI("%d\n", i);
+    }
+}
