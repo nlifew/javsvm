32ceac4549b8ed54962c0f5d018471b711b10068
nlifew <nlifew.ss@gmail.com>
2022-05-02 10:27:14 +0800

    test:【main】新增对 main(String[]) 的调用测试

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,37 @@
 
 
 #include "../vm/jvm.h"
-#include "../object/jclass.h"
 #include "../object/jmethod.h"
+#include "../engine/engine.h"
 
 using namespace javsvm;
 
-int main()
+
+
+static void invoke_main(const char *main_method) noexcept
+{
+    auto &vm = jvm::get();
+
+    auto klass = vm.bootstrap_loader.load_class(main_method);
+    assert(klass != nullptr);
+
+    auto main = klass->get_static_method("main", "([Ljava/lang/String;)V");
+    assert(main != nullptr);
+
+    // String 类
+    auto java_lang_String = vm.bootstrap_loader.load_class("java/lang/String");
+    assert(java_lang_String != nullptr);
+
+    // 解析需要传递到 java 层的参数
+    slot_t slots[1];
+    jargs args = slots;
+    args.next<jref>() = vm.array.new_object_array(java_lang_String, 0);
+
+    // 执行 main 函数
+    main->invoke_static(args);
+}
+
+int main(int argc, const char *argv[])
 {
     putenv(strdup("CLASSPATH=../../jre/out"));
 
@@ -18,6 +43,9 @@ int main()
 
     auto ok = java_lang_System->invoke_clinit();
     assert(ok == 0);
+
+    assert(argc == 2);
+    invoke_main(argv[1]);
 
     vm.detach();
     vm.wait_for();
